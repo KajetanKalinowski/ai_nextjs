27890b0014917a311cc8b6ccaaee942177a68c13
KajetanKalinowski <125036724+KajetanKalinowski@users.noreply.github.com>
2025-04-16 08:45:16 +0200

    logowanie git

---
+++ app/src/app/logowanie/page.js
@@ -0,0 +1,71 @@
+'use client'
+import { useState,useEffect } from "react"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  import { Input } from "@/components/ui/input"
+  import { Label } from "@/components/ui/label"
+  import { Button } from "@/components/ui/button"
+  import PocketBase, { ClientResponseError } from 'pocketbase';
+const pb = new PocketBase('http://172.16.15.138:8080');
+export default function Log() {
+    const [inputs, setInputs] = useState({login:null,password:null})
+    const handleLogin = async ()=>{
+        try{
+            const authData = await pb.collection('users').authWithPassword(
+                inputs.login,
+                inputs.password,
+            );
+            if(typeof window !== "undefined"){
+                
+                  window.location.href="/"
+                
+                //   const sensitiveData = {oldPass: inputs.password}
+                //   sessionStorage.setItem('sensitiveData', JSON.stringify(sensitiveData));
+                //   router.push('/userVerify');
+                console.log("dobrze")
+                  
+            }else{
+                console.log("zle")
+            }
+        }catch(err){
+            console.log(err)
+        }}
+    const handleInput = (e,target)=>{
+        setInputs((prev)=>({...prev,[target]:e.target.value}))
+    }
+    const handleKeyPress = (event) => {
+      if (event.key === 'Enter') {
+        handleLogin(); // Wykonaj akcję, jak przy kliknięciu przycisku
+      }
+    };
+    return(
+        <div className="flex flex-col justify-center items-center w-full h-[90vh]">
+        <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle className="flex justify-center">Logowanie</CardTitle>
+      </CardHeader>
+      <CardContent>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="login" className="flex flex-row text-center items-center justify-between">E-mail</Label>
+              <Input id="login" placeholder="Login" onChange={(e)=>{handleInput(e,"login")}} onKeyDown={handleKeyPress}/>
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="pass" className="flex flex-row text-center items-center justify-between">Password</Label>
+              <Input type="password" id="pass" placeholder="Password" onChange={(e)=>{handleInput(e,"password")}} onKeyDown={handleKeyPress}/>
+            </div>
+          </div>
+      </CardContent>
+      <CardFooter className="flex justify-center">
+        <Button onClick={handleLogin}>Zaloguj</Button>
+      </CardFooter>
+    </Card>
+    </div>
+    )
+}
--- app/src/app/page.js
+++ app/src/app/page.js
@@ -12,8 +12,8 @@ export default function Home(){
   const [history,setHistory]=useState(null)
     const getData = async ()=>{
     try {
-    //  const data = await fetch('http://172.16.15.138:5678/webhook/api',{headers:{"topic":`${input}`}})
-     const data = await fetch('http://192.168.0.136:5678/webhook/api',{headers:{"topic":`${input}`}})
+      const data = await fetch('http://172.16.15.138:5678/webhook/api',{headers:{"topic":`${input}`}})
+     //const data = await fetch('http://192.168.0.136:5678/webhook/api',{headers:{"topic":`${input}`}})
       const json = await data.json()
       console.log(json.output)
       setDane(json.output)
@@ -31,8 +31,8 @@ export default function Home(){
   useEffect(()=>{
     const getHis = async()=>{
       try {
-        // const data = await fetch('http://172.16.15.138:5678/webhook/history')
-        const data = await fetch('http://192.168.0.136:5678/webhook/history')
+         const data = await fetch('http://172.16.15.138:5678/webhook/history')
+       // const data = await fetch('http://192.168.0.136:5678/webhook/history')
         const json = await data.json()
         console.log(json.items)
         setHistory(json.items)
@@ -49,8 +49,8 @@ export default function Home(){
   }
   const sendInf = async(i)=>{
     console.log(i)
-    // await fetch(`http://172.16.15.138:5678/webhook/base?question=${dane.question}&answer1=${dane.answers[0].text}&answer2=${dane.answers[1].text}&usr_answer=${i}&correct_answer=${(dane.answers[0].isCorrect==true?dane.answers[0].text:dane.answers[1].text)}`,{method:"POST"})
-    await fetch(`http://192.168.0.136:5678/webhook/base?question=${dane.question}&answer1=${dane.answers[0].text}&answer2=${dane.answers[1].text}&usr_answer=${i}&correct_answer=${(dane.answers[0].isCorrect==true?dane.answers[0].text:dane.answers[1].text)}`,{method:"POST"})
+    await fetch(`http://172.16.15.138:5678/webhook/base?question=${dane.question}&answer1=${dane.answers[0].text}&answer2=${dane.answers[1].text}&usr_answer=${i}&correct_answer=${(dane.answers[0].isCorrect==true?dane.answers[0].text:dane.answers[1].text)}`,{method:"POST"})
+    //await fetch(`http://192.168.0.136:5678/webhook/base?question=${dane.question}&answer1=${dane.answers[0].text}&answer2=${dane.answers[1].text}&usr_answer=${i}&correct_answer=${(dane.answers[0].isCorrect==true?dane.answers[0].text:dane.answers[1].text)}`,{method:"POST"})
     setClick(1)
   }
   
@@ -71,8 +71,8 @@ export default function Home(){
           </div>
       }
       <ScrollArea className="flex flex-col gap-2 h-[50vh] w-full rounded-md border">
-        {history&&history.map((item)=>(
-          <div className="flex flex-col justify-center items-center gap-2 m-2">
+        {history&&history.map((item,idx)=>(
+          <div key={idx} className="flex flex-col justify-center items-center gap-2 m-2">
           <h1>{item.question}</h1>
           <Button disabled variant="outline" className={item.correct_answer==item.answer1?`border-2 border-black bg-green-500 disabled:opacity-100`:`border-2 border-black bg-red-500 disabled:opacity-100`}>{item.answer1}</Button>
           <Button disabled variant="outline" className={item.correct_answer==item.answer2?`border-2 border-black bg-green-500 disabled:opacity-100`:`border-2 border-black bg-red-500 disabled:opacity-100`}>{item.answer2}</Button>
