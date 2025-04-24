ee04f03789e4c2bff784d92985cb36ea99882fa4
KajetanKalinowski <125036724+KajetanKalinowski@users.noreply.github.com>
2025-04-24 17:28:17 +0200

    wow

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -3,6 +3,15 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { useState,useEffect } from "react"
 import { ScrollArea } from "@/components/ui/scroll-area"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 import PocketBase, { ClientResponseError } from 'pocketbase';
 const pb = new PocketBase('http://172.16.15.138:8080');
 export default function Home(){
@@ -12,6 +21,7 @@ export default function Home(){
   const [click,setClick]=useState(0) 
   const [history,setHistory]=useState(null)
   const [user,setUser] = useState(pb.authStore.record)
+  const [show,setShow] = useState(true)
   if(!user){
     window.location.href="/logowanie"
   }
@@ -20,13 +30,14 @@ export default function Home(){
       const data = await fetch('http://172.16.15.138:5678/webhook/api',{headers:{"topic":`${input}`}})
      //const data = await fetch('http://192.168.0.136:5678/webhook/api',{headers:{"topic":`${input}`}})
       const json = await data.json()
-      console.log(json.output)
+      console.log(data)
+      console.log(json)
       setDane(json.output)
       setClick(0)
       setFirst(1)
-      if(json.output==undefined){
-        getData()
-    }
+    //   if(json.output==undefined){
+    //     getData()
+    // }
     
     } catch (err) {
       console.log(err)
@@ -50,11 +61,13 @@ export default function Home(){
 
   
   const handleInput = (e)=>{
-    setInput(e.target.value)
+    setInput(e)
+    console.log(e)
+    setShow(false)
   }
   const sendInf = async(i)=>{
     console.log(i)
-    await fetch(`http://172.16.15.138:5678/webhook/base?question=${dane.question}&answer1=${dane.answers[0].text}&answer2=${dane.answers[1].text}&usr_answer=${i}&correct_answer=${(dane.answers[0].isCorrect==true?dane.answers[0].text:dane.answers[1].text)}`,{method:"POST"})
+    await fetch(`http://172.16.15.138:5678/webhook/base?question=${dane.question}&answer1=${dane.answers[0].text}&answer2=${dane.answers[1].text}&answer3=${dane.answers[2].text}&answer4=${dane.answers[3].text}&usr_answer=${i}&correct_answer=${(dane.answers[0].isCorrect==true?dane.answers[0].text:(dane.answers[1].isCorrect==true?dane.answers[1].text:(dane.answers[2].isCorrect==true?dane.answers[2].text:(dane.answers[3].isCorrect==true?dane.answers[3].text:null))))}`,{method:"POST"})
     //await fetch(`http://192.168.0.136:5678/webhook/base?question=${dane.question}&answer1=${dane.answers[0].text}&answer2=${dane.answers[1].text}&usr_answer=${i}&correct_answer=${(dane.answers[0].isCorrect==true?dane.answers[0].text:dane.answers[1].text)}`,{method:"POST"})
     setClick(1)
   }
@@ -65,14 +78,30 @@ export default function Home(){
   return(
     <div className="flex flex-col justify-center items-center h-screen w-screen gap-2">
       <Button onClick={wyloguj}>Wyloguj</Button>
-      <Input onChange={(e)=>{handleInput(e)}} placeholder="Temat pytania" className="w-[300px]"></Input>
-      {first==0?<Button onClick={getData}>Losuj Pytanie</Button>:<></>}
+      {/* <Input onChange={(e)=>{handleInput(e)}} placeholder="Temat pytania" className="w-[300px]"></Input> */}
+      <Select onValueChange={(e)=>{handleInput(e)}}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Kategorie Pytań" />
+      </SelectTrigger>
+      <SelectContent >
+        <SelectGroup>
+          <SelectLabel>Języki programowania</SelectLabel>
+          <SelectItem value="HTML">HTML</SelectItem>
+          <SelectItem value="CSS">CSS</SelectItem>
+          <SelectItem value="JavaScript">JavaScript</SelectItem>
+          <SelectItem value="Python">Python</SelectItem>
+          <SelectItem value="C#">C#</SelectItem>
+          <SelectItem value="C++">C++</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+      {first==0?<Button onClick={getData} disabled={show}>Losuj Pytanie</Button>:<></>}
       {dane && 
           <div className="flex flex-col justify-start items-center gap-2">
             <h1 className="font-bold">{dane.question}</h1>
             <div className="flex flex-col gap-2">
-            {dane.answers && dane.answers.map((item)=>(
-              <Button disabled={click==1?true:false} variant="outline" key={item.isCorrect} onClick={()=>{sendInf(item.isCorrect)}} className={click==1?(item.isCorrect==true?`border-2 border-black bg-green-500 disabled:opacity-100`:`border-2 border-black bg-red-500 disabled:opacity-100`):`border-2 border-black bg-black-500`}>{item.text}</Button>
+            {dane.answers && dane.answers.map((item,idx)=>(
+              <Button disabled={click==1?true:false} variant="outline" key={idx} onClick={()=>{sendInf(item.isCorrect)}} className={click==1?(item.isCorrect==true?`border-2 border-black bg-green-500 disabled:opacity-100`:`border-2 border-black bg-red-500 disabled:opacity-100`):`border-2 border-black bg-black-500`}>{item.text}</Button>
             ))}
             
             </div>
@@ -85,6 +114,8 @@ export default function Home(){
           <h1>{item.question}</h1>
           <Button disabled variant="outline" className={item.correct_answer==item.answer1?`border-2 border-black bg-green-500 disabled:opacity-100`:`border-2 border-black bg-red-500 disabled:opacity-100`}>{item.answer1}</Button>
           <Button disabled variant="outline" className={item.correct_answer==item.answer2?`border-2 border-black bg-green-500 disabled:opacity-100`:`border-2 border-black bg-red-500 disabled:opacity-100`}>{item.answer2}</Button>
+          <Button disabled variant="outline" className={item.correct_answer==item.answer3?`border-2 border-black bg-green-500 disabled:opacity-100`:`border-2 border-black bg-red-500 disabled:opacity-100`}>{item.answer3}</Button>
+          <Button disabled variant="outline" className={item.correct_answer==item.answer4?`border-2 border-black bg-green-500 disabled:opacity-100`:`border-2 border-black bg-red-500 disabled:opacity-100`}>{item.answer4}</Button>
           <div className="w-full border-2 border-black"></div>
           </div>
         ))}
